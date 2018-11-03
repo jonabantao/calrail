16ce624994b2a79c96493d6bf1fcc9e452ee52db
Jonathan Abantao <jonabantao@gmail.com>
2018-11-02 21:31:32 -0700

    Set up backend for async/await

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 const STATIC_PATH = path.join(__dirname, '/frontend/build');
+const queries = require('./query');
 
 // Middleware
 app.use(cors());
+app.use(express.json());
 app.use(express.static(STATIC_PATH));
+
+app.get('/api/users', async (req, res) => {
+  try {
+    let response = await queries.fetchEmployees();
+    console.log(response);
+
+    res.status(200).json(response);
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(404);
+  }
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${STATIC_PATH}/index.html`));
